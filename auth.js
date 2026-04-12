a0813361b47cbe8d24a2a5140ab0d524ed2bce20
mahmudtech <mahmud.irfan.dev@gmail.com>
2026-04-12 23:06:46 +0600

    credentials auth config set

--- auth.js
+++ auth.js
@@ -1,5 +1,6 @@
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import mongoClientPromise from "./database/mongoClientPromise";
 
@@ -13,6 +14,33 @@ export const {
     databaseName: process.env.ENVIRONMENT,
   }),
   providers: [
+    CredentialsProvider({
+      credentials: {
+        email: {},
+        password: {},
+      },
+
+      async authorize(credentials) {
+        if (credentials == null) return null;
+
+        try {
+          const user = await userModel.findOne({ email: credentials.email });
+          console.log({ user });
+          if (user) {
+            const isMatch = user.email === credentials.email;
+            if (isMatch) {
+              return user;
+            } else {
+              throw new Error("Email or password mismatch");
+            }
+          } else {
+            throw new Error("User not found");
+          }
+        } catch (error) {
+          throw new Error(error);
+        }
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
