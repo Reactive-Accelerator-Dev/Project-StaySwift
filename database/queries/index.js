ffd767e3e384b6ce7dff64f1aeac3eac5c9aeb6d
mahmudtech <mahmud.irfan.dev@gmail.com>
2026-04-13 15:47:21 +0600

    finding bookig logic get search reslt cehek in and chek out data show data

--- database/queries/index.js
+++ database/queries/index.js
@@ -1,11 +1,17 @@
+import { bookingModel } from "@/models/booking-model";
 import { hotelModel } from "@/models/hotel-model";
 import { ratingModel } from "@/models/rating-model";
 import { reviewModel } from "@/models/review-model";
-import { replaceMongoIdInArray, replaceMongoIdInObject } from "@/utils/data-util";
+import {
+    isDateInbetween,
+  replaceMongoIdInArray,
+  replaceMongoIdInObject,
+} from "@/utils/data-util";
 
-export async function getAllHotels() {
-  const hotels = await hotelModel
-    .find()
+export async function getAllHotels(destination, checkin, checkout) {
+  const regex = new RegExp(destination, "i");
+  const hotelsByDestination = await hotelModel
+    .find({ city: { $regex: regex } })
     .select([
       "thumbNailUrl",
       "name",
@@ -16,7 +22,40 @@ export async function getAllHotels() {
     ])
     .lean();
 
-  return replaceMongoIdInArray(hotels);
+  let allHotels = hotelsByDestination;
+
+  if (checkin && checkout) {
+    allHotels = await Promise.all(
+      allHotels.map(async (hotel) => {
+        const found = await findBooking(hotel._id, checkin, checkout);
+        console.log(found);
+        if (found) {
+          hotel["isBooked"] = true;
+        } else {
+          hotel["isBooked"] = false;
+        }
+        return hotel;
+      }),
+    );
+  }
+
+  return replaceMongoIdInArray(allHotels);
+}
+
+async function findBooking(hotelId, checkin, checkout) {
+  const matches = await bookingModel
+    .find({ hotelId: hotelId.toString() })
+    .lean();
+
+  const found = matches.find((match) => {
+    return (
+      isDateInbetween(checkin, match.checkin, match.checkout) ||
+      isDateInbetween(checkout, match.checkin, match.checkout)
+    );
+  });
+  console.log(found);
+
+  return found;
 }
 
 export async function getHotelById(hotelId) {
