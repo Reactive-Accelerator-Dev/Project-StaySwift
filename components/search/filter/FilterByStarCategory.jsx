cb07f81697001723853e24fb6f3c7fcc2985f6e7
mahmudtech <mahmud.irfan.dev@gmail.com>
2026-04-16 16:14:18 +0600

    params get

--- components/search/filter/FilterByStarCategory.jsx
+++ components/search/filter/FilterByStarCategory.jsx
@@ -1,26 +1,37 @@
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
 export default function FilterByStarCategory() {
+  const [query, setQuery] = useState([]);
+
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const params = new URLSearchParams(searchParams);
+
   return (
     <div>
       <h3 className="font-bold text-lg">Star Category</h3>
       <form action="" className="flex flex-col gap-2 mt-2">
         <label for="fiveStar">
-          <input type="checkbox" name="fiveStar" id="fiveStar" />5 Star
+          <input type="checkbox" name="5" id="fiveStar" />5 Star
         </label>
 
         <label for="fourStar">
-          <input type="checkbox" name="fourStar" id="fourStar" />4 Star
+          <input type="checkbox" name="4" id="fourStar" />4 Star
         </label>
 
         <label for="threeStar">
-          <input type="checkbox" name="threeStar" id="threeStar" />3 Star
+          <input type="checkbox" name="3" id="threeStar" />3 Star
         </label>
 
         <label for="twoStar">
-          <input type="checkbox" name="twoStar" id="twoStar" />2 Star
+          <input type="checkbox" name="2" id="twoStar" />2 Star
         </label>
 
         <label for="oneStar">
-          <input type="checkbox" name="oneStar" id="oneStar" />1 Star
+          <input type="checkbox" name="1" id="oneStar" />1 Star
         </label>
       </form>
     </div>
