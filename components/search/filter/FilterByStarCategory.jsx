07fdd548f170ae517a77eb2a0a18c12223dd9a8a
mahmudtech <mahmud.irfan.dev@gmail.com>
2026-04-16 16:20:17 +0600

    query console log

--- components/search/filter/FilterByStarCategory.jsx
+++ components/search/filter/FilterByStarCategory.jsx
@@ -10,28 +10,73 @@ export default function FilterByStarCategory() {
   const { replace } = useRouter();
   const params = new URLSearchParams(searchParams);
 
+  const handleChange = (event) => {
+    event.preventDefault();
+    const name = event.target.name;
+    const checked = event.target.checked;
+
+    if (checked) {
+      setQuery((prev) => [...prev, name]);
+    } else {
+      const filtered = query.filter((item) => item !== name);
+      setQuery(filtered);
+    }
+
+    console.log(query);
+  };
+
   return (
     <div>
       <h3 className="font-bold text-lg">Star Category</h3>
       <form action="" className="flex flex-col gap-2 mt-2">
         <label for="fiveStar">
-          <input type="checkbox" name="5" id="fiveStar" />5 Star
+          <input
+            onChange={handleChange}
+            type="checkbox"
+            name="5"
+            id="fiveStar"
+          />
+          5 Star
         </label>
 
         <label for="fourStar">
-          <input type="checkbox" name="4" id="fourStar" />4 Star
+          <input
+            onChange={handleChange}
+            type="checkbox"
+            name="4"
+            id="fourStar"
+          />
+          4 Star
         </label>
 
         <label for="threeStar">
-          <input type="checkbox" name="3" id="threeStar" />3 Star
+          <input
+            onChange={handleChange}
+            type="checkbox"
+            name="3"
+            id="threeStar"
+          />
+          3 Star
         </label>
 
         <label for="twoStar">
-          <input type="checkbox" name="2" id="twoStar" />2 Star
+          <input
+            onChange={handleChange}
+            type="checkbox"
+            name="2"
+            id="twoStar"
+          />
+          2 Star
         </label>
 
         <label for="oneStar">
-          <input type="checkbox" name="1" id="oneStar" />1 Star
+          <input
+            onChange={handleChange}
+            type="checkbox"
+            name="1"
+            id="oneStar"
+          />
+          1 Star
         </label>
       </form>
     </div>
